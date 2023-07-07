0ffdc9dc8557180d557e6e2d0e35ccff813204e8
Destany <destany@zipcoders-mbp.mynetworksettings.com>
2023-07-07 18:15:13 -0400

    Completed

--- TicketMachineTest.java
+++ TicketMachineTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
  * @author  (your name)
  * @version (a version number or a date)
  */
-public class TicketMachineTest
+public class TicketMachineTest 
 {
     /**
      * Default constructor for test class TicketMachineTest
